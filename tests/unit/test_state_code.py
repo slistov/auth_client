a6479114aec5f295ac177ffbb3729fe17c5bb48e
Sergey <slistov@mail.ru>
2022-09-07 14:55:08 +0300

    Tests Code sent, token recieved

--- tests/unit/test_state_code.py
+++ tests/unit/test_state_code.py
@@ -1,5 +1,46 @@
-def test_state_generated_and_saved_in_db():
-    state_out = State()
+class FakeAuthService():
+    def __init__(self, grant) -> None:
+        self.grant = grant
+   
+    def _get_token_for_authCode_grant(self):
+        return 'test_token_by_authCode_grant'
 
-    state_in = State(state_out.code)
-    assert state_out == state_in
+
+    def _get_token_for_refresh_grant(self):
+        return 'test_token_by_refresh_grant'
+
+
+    _get_token_for_grant = {
+        'authorization_code': _get_token_for_authCode_grant,
+        'refresh_token': _get_token_for_refresh_grant
+    }
+
+    def get_token(self):
+        return self._get_token_for_grant[self.grant['type']](self)
+
+
+class TokenService():
+    access_token: str = None
+    def __init__(self, auth_service) -> None:
+        self.auth_service = auth_service
+
+    def get_access_token(self):
+        if not self.access_token:
+            self.access_token = self.auth_service.get_token()
+        return self.access_token
+
+
+def test__TokenService__sends_authCode__recieves_token_from_AuthService():
+    auth_service = FakeAuthService(grant={'type':'authorization_code', 'code':'test_code'})
+    token_service = TokenService(auth_service=auth_service)
+    access_token = token_service.get_access_token()
+
+    assert access_token == "test_token_by_authCode_grant"
+
+
+def test__TokenService__sends_refreshToken__recieves_token_from_AuthService():
+    auth_service = FakeAuthService(grant={'type':'refresh_token', 'code':'test_code'})
+    token_service = TokenService(auth_service=auth_service)
+    access_token = token_service.get_access_token()
+
+    assert access_token == "test_token_by_refresh_grant"
