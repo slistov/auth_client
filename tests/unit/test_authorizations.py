0a8d21eb144902666f38a919f062450b3fab99eb
Sergey <slistov@mail.ru>
2022-09-30 11:01:19 +0300

    small refact

--- tests/unit/test_authorizations.py
+++ tests/unit/test_authorizations.py
@@ -1,7 +1,7 @@
 from src.auth_client.domain import model
 
 
-def test_for_auth_deactivate_deactivates_authorization_and_its_state_grants_and_tokens():
+def test_when_authorization_deactivates_it_also_deactivates_its_state_grants_and_tokens():
     state = model.State("test_state")
     grant = model.Grant("authorization_code", "test_code")
     token = model.Token("test_token")
