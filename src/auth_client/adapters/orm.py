f03ca4f9f7c82e33f6b7c6b73eb2b9a2d5b21690
Sergey <slistov@mail.ru>
2022-10-19 12:17:58 +0300

    ORM - again as one module orm.py - alembic script tested

--- src/auth_client/adapters/orm.py
+++ src/auth_client/adapters/orm.py
@@ -7,13 +7,39 @@ from sqlalchemy import (
     Interval,
     Boolean,
     ForeignKey,
+    FetchedValue
 )
+from sqlalchemy.dialects.postgresql import JSONB
+# Из-за проблем при редактировании json "по месту",
+# будем юзать эту либу
+# https://amercader.net/blog/beware-of-json-fields-in-sqlalchemy/
+from sqlalchemy_json import mutable_json_type
 
-from sqlalchemy.orm import relationship, registry
-from .orm import mapper_registry
 from auth_client.domain import model
+from sqlalchemy import event
+
+from sqlalchemy.orm import registry, relationship
 
 mapper_registry = registry()
+
+users = Table(
+    'users', mapper_registry.metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True, server_default=FetchedValue()),
+    Column('email', String),
+    Column('username', String),    
+    Column('created', DateTime),
+    Column('is_active', Boolean)
+)
+
+desks = Table(
+    'desks', mapper_registry.metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True, server_default=FetchedValue()),
+    Column('name', String),
+    Column('structure_json', mutable_json_type(dbtype=JSONB, nested=True)),    
+    Column('created', DateTime),
+    Column('created_by', Integer, ForeignKey('users.id'), nullable=False),    
+    Column('is_active', Boolean)
+)
 
 authorizations = Table(
     'authorizations', mapper_registry.metadata,
@@ -52,7 +78,15 @@ tokens = Table(
 )
 
 
-def start_oauth_mappers():
+def start_mappers():
+    users_mapper = mapper_registry.map_imperatively(model.User, users)    
+    desks_mapper = mapper_registry.map_imperatively(
+        model.Desk, 
+        desks,
+        properties={
+            "user": relationship(users_mapper, uselist=False),
+        }        
+    )
     states_mapper = mapper_registry.map_imperatively(model.State, states)
     grants_mapper = mapper_registry.map_imperatively(model.Grant, grants)    
     tokens_mapper = mapper_registry.map_imperatively(model.Token, tokens)
@@ -64,4 +98,11 @@ def start_oauth_mappers():
             "grants": relationship(grants_mapper),
             "tokens": relationship(tokens_mapper)
         }        
-    ) 
+    )
+
+@event.listens_for(model.Authorization, "load")
+def receive_load(auth, _):
+    auth.events = []
+
+
+
--- src/auth_client/domain/model/user.py
+++ src/auth_client/domain/model/user.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 class User:
     def __init__(self, email: str, username: str):
         self.email = email
         self.username = username
-        
+        self.created = datetime.utcnow()
+        self.is_active = True
