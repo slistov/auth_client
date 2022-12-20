431e669c691520729eed267d0c362d30c8967a31
Sergey <slistov@mail.ru>
2022-12-20 14:56:10 +0300

    requester removed

--- tests/unit/test_desks.py
+++
@@ -1,7 +0,0 @@
-from oauth_client_lib.domain import model
-
-def test_desk():
-    desk = model.Desk("Desk", {'struct': 'test struct'})
-
-    assert desk.name == 'Desk'
-    assert desk.structure_json == {'struct': 'test struct'}
