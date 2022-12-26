45d4218cb68e3fe13666031f5122758893d55bd6
Sergey <slistov@mail.ru>
2022-12-26 15:16:54 +0300

    Tests - test_oauth_provider errors

--- tests/unit/test_oauth_provider.py
+++ tests/unit/test_oauth_provider.py
@@ -2,7 +2,6 @@ import pytest
 from src.oauth_client_lib.service_layer.oauth_provider import OAuthProvider
 from urllib.parse import urlparse, parse_qs
 
-
 class TestOAuthProvider:
     def test_creation(self, test_provider):
         assert test_provider.name == 'test_oauth_provider'
@@ -15,8 +14,8 @@ class TestOAuthProvider:
         assert test_provider.public_keys_url == 'https://www.testapis.com/oauth2/v3/certs'
 
     @pytest.mark.asyncio
-    async def test_returns_authorize_uri(self, test_provider: OAuthProvider):
-        assert await test_provider.get_authorize_uri() == 'sdf'
+    async def test_returns_authorize_uri(self, uow, test_provider: OAuthProvider):
+        assert await test_provider.get_authorize_uri(uow=uow) == 'https://accounts.test.com/o/oauth2/v2/auth?response_type=code&client_id=test_client_id&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Foauth%2Fcallback&scope=%5B%27https%3A%2F%2Fwww.testapis.com%2Fauth%2Fuserinfo.email%27%2C+%27openid%27%5D&state=some_state_code'
 
     def test_authorize_uri_contains_state(self, test_provider: OAuthProvider):
         url = test_provider.get_authorize_uri(state='test_state')
