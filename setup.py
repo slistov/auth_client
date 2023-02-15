82476ec2641f59a15985ab549cdabe67fce27308
Sergey <slistov@mail.ru>
2023-02-15 14:38:33 +0300

    30

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ with open("README.md", encoding='utf8') as file:
 
 setup(
     name="oauth-client-lib",
-    version="0.1.26",
+    version="0.1.30",
     author="Sergey Listov",
     author_email="slistov@mail.ru",
     description="OAuth2 client library",
     long_description=read_me_description,
     long_description_content_type="text/markdown",
     url="https://github.com/slistov/oauth_client_lib.git",
-    # package_dir={'': 'src'},
-    packages=find_packages(exclude=['tests']),
+    package_dir={'': 'src'},
+    packages=find_packages(
+        include=['oauth_client_lib', 'oauth_client_lib.*', ]
+    ),
     install_requires=[
         'pytest',
         'pytest-asyncio',
