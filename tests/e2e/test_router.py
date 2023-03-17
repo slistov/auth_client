f3d91f02a5bddd41599ed43b30c50146c02b765e
slistov <slistov@mail.ru>
2023-03-17 17:33:39 +0300

    Tests (micro refact) Comment added

--- tests/e2e/test_router.py
+++ tests/e2e/test_router.py
@@ -4,7 +4,9 @@ from urllib.parse import urlparse, parse_qsl, unquote, urlsplit
 
 class TestRouter:
     @pytest.mark.asyncio
-    async def test_oauth_redirect(self, client):
+    async def test_oauth_redirect_URL_correct(self, client):
+        """Redirect url must match specifications
+        https://www.oauth.com/oauth2-servers/authorization/the-authorization-request/"""
         r = client.get("http://testserver/oauth/redirect?provider=fake_provider")
 
         url_query = urlsplit(r.url).query
