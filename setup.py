80d870326c093683c235b3bf07f9d802d79483e3
slistov <slistov@mail.ru>
2023-04-03 11:06:50 +0300

    ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf8") as file:
 
 setup(
     name="oauth-client-lib",
-    version="0.1.38",
+    version="0.1.40",
     author="Sergey Listov",
     author_email="slistov@mail.ru",
     description="OAuth2 client library",
